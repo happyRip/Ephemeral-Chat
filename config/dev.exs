d51debffb37896c720036971d2699ac00f5bb64c
Aleksander Borowski <aborbzbk@gmail.com>
2022-05-03 14:23:18 +0200

    Add https support

--- config/dev.exs
+++ config/dev.exs
@@ -19,7 +19,13 @@ config :ephemeral_chat, EphemeralChat.Repo,
 config :ephemeral_chat, EphemeralChatWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [port: 4000],
+  https: [
+    port: 4001,
+    cipher_suite: :strong,
+    keyfile: "priv/cert/selfsigned_key.pem",
+    certfile: "priv/cert/selfsigned.pem"
+  ],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
