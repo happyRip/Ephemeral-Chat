b55f08922e103a7036854a698a3c9f65bb0edaf3
Aleksander Borowski <aborbzbk@gmail.com>
2022-05-11 22:02:57 +0200

    Move Messager to separate file

--- lib/ephemeral_chat_web/live/room_live.ex
+++ lib/ephemeral_chat_web/live/room_live.ex
@@ -3,6 +3,9 @@ defmodule EphemeralChatWeb.RoomLive do
   alias EphemeralChatWeb, as: ChatWeb
   alias ChatWeb.{Endpoint, Presence}
 
+  require EphemeralChat.Messager
+  alias EphemeralChat.Messager
+
   @impl true
   def mount(%{"id" => room_id}, _session, socket) do
     topic = "room:" <> room_id
@@ -116,43 +119,3 @@ defmodule EphemeralChatWeb.RoomLive do
   end
 end
 
-defmodule Messager do
-  def new_message(content, author \\ :system)
-
-  def new_message(content, :system) do
-    %{uuid: UUID.uuid4(), content: content, author: :system}
-  end
-
-  def new_message(content, author) do
-    %{uuid: UUID.uuid4(), content: content, author: author}
-  end
-
-  def append(messages, []), do: messages
-
-  def append([%{author: author, uuid: uuid, messages: messages} | tail], message)
-      when author == message.author do
-    message = Map.delete(message, :author)
-
-    [%{author: author, uuid: uuid, messages: [message | messages]} | tail]
-  end
-
-  def append(%{author: author, uuid: uuid, messages: messages}, message)
-      when author == message.author do
-    message = Map.delete(message, :author)
-
-    [%{author: author, uuid: uuid, messages: [message | messages]}]
-  end
-
-  def append(messages, message) do
-    [new_container(message) | messages]
-  end
-
-  def new_container(), do: %{}
-
-  def new_container(message) do
-    uuid = UUID.uuid4()
-    {author, message} = Map.pop(message, :author)
-
-    %{author: author, uuid: uuid, messages: [message]}
-  end
-end
